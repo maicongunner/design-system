7fb0664cf0ec6473616fdad018144d0a3b2f82c2
maicon.gunner <maikonbdomingues@gmail.com>
2022-11-10 12:33:51 -0300

    feat: implements to show the all colors project from storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,32 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
 export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
+  backgroundColor: '$ignite300',
   borderRadius: '$md',
   height: '$10',
-  padding: '$4',
+  border: 0,
+  padding: '$2 $4',
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
+
+export type ButtonProps = ComponentProps<typeof Button>
