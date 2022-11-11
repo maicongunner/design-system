f9f7b80b1c908f642d681cbeb1516c3e73e12594
maicon.gunner <maikonbdomingues@gmail.com>
2022-11-11 09:44:56 -0300

    feat: adds the constrols, actions and accessibility from storybook components

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/maicongunner.png',
     alt: 'Maicon Domingues',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -15,6 +15,15 @@ export const CustomTag: StoryObj<HeadingProps> = {
   args: {
     children: 'H1 Heading',
     as: 'h1',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg', '2xl', '4xl', '5xl', '6xl'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
   parameters: {
     docs: {
